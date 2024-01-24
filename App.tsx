f0fe2c083d82a1d16683ed419b4059480fcd39cb
alexRicc2 <alex.65.santanna@gmail.com>
2024-01-23 23:27:21 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  useColorScheme,
+  Text,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView contentInsetAdjustmentBehavior="automatic" />
+      <Text>Olaaa</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
